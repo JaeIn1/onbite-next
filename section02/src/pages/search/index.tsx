899fca6f82b879adb1d1c798d169c4c7432f9c08
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-11-30 17:30:49 +0900

    feat : 클라이언트에서만 query-string을 반영 가능 , 클라이언트에서 처리

--- section02/src/pages/search/index.tsx
+++ section02/src/pages/search/index.tsx
@@ -1,33 +1,49 @@
 import { ReactNode, useEffect, useState } from "react";
 import SearchableLayout from "@/components/searchable-layout/searchable-layout";
 import BookItem from "@/components/book-layout/book-item";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+// import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
+import { BookData } from "@/types";
+import { useRouter } from "next/router";
 import Head from "next/head";
 
-export const getServerSideProps = async (
-  content: GetServerSidePropsContext
-) => {
-  const q = content.query.q;
+// export const getStaticProps = async (
+//   content: GetServerSidePropsContext
+// ) => {
+//   const q = content.query.q;
 
-  const books = await fetchBooks(q as string);
+//   const books = await fetchBooks(q as string);
 
-  return {
-    props: {
-      books,
-    },
+//   return {
+//     props: {
+//       books,
+//     },
+//   };
+// };
+
+export default function Page() {
+  const [books, setBooks] = useState<BookData[]>([]);
+
+  const router = useRouter();
+  const q = router.query.q;
+
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
   };
-};
 
-export default function Page({
-  books,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  useEffect(() => {
+    if (q) {
+      fetchSearchResult();
+    }
+  }, [q]);
+
   return (
     <>
       <Head>
-        <title>한입북스 - 검색결과</title>
+        <title>한입북스 - 검색 결과</title>
         <meta property="og:image" content="/thumbnail.png" />
-        <meta property="og:title" content="한입북스 - 검색결과" />
+        <meta property="og:title" content="한입북스" />
         <meta
           property="og:description"
           content="한 입 북스에 등록된 도서를 만나보세요"
